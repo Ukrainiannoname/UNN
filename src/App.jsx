2951264d5b60f6bab7c3deb84c6295b406d3672d
Ukrainiannoname <serhiy6788@gmail.com>
2025-04-23 12:07:12 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
-import useReveal from './hooks/useReveal';
 import HomePage from './pages/HomePage';
 import QuizPage from './pages/QuizPage';
-import SizeForm from './pages/SizeForm';
 import ProductsPage from './pages/ProductsPage';
 import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
+import SizeForm from './pages/SizeForm';
 
-function App() {
-  useReveal();
-
+export default function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/size-form" element={<SizeForm />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/size-form" element={<SizeForm />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
+// src/components/Header.jsx
 import { Link } from 'react-router-dom';
 import '../styles/styles.css';
 
 function Header() {
   return (
-    <header className="site-header">
+    <header className="site-header reveal">
       <div className="container">
         <h1 className="logo">UNN</h1>
         <nav className="nav">
